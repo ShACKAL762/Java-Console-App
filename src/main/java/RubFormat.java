03ee3722dbdde646fe2997b4ee8aa0052d6ad233
Sergey <Cer.97@yandex.ru>
2023-10-06 13:12:49 +0300

    Исправил форматирование для 11-14 рублей

--- src/main/java/RubFormat.java
+++ src/main/java/RubFormat.java
@@ -1,8 +1,12 @@
 public class RubFormat {
     protected static String format(double sum) {
+        int x;
 
+        if(sum >=11 && sum < 15)
+            x = (int) sum %100;
+        else
+            x = (int) sum % 10;
 
-        int x = (int) sum % 10;
         return switch (x) {
             case 1 -> "рубль.";
             case 2, 3, 4 -> "рубля.";
